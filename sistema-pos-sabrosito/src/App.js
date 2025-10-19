286059082e137c3869ee17374aa5c52e060bf364
Jhon Heynar Alvarez <tu-correo@ejemplo.com>
2025-10-18 23:04:02 -0400

    Actualización completa del sistema Sabrosito

--- sistema-pos-sabrosito/src/App.js
+++ sistema-pos-sabrosito/src/App.js
@@ -130,9 +130,9 @@ export default function App() {
   const generarTextoTicket = () => {
     let texto = "\x1B\x61\x01"; // centrar 
     texto +=  "\x1B\x46\x01"; // Negrilla ON
-    texto += "\x1B\x21\x29"; // Formato grande
+    texto += "\x1B\x21\x31"; // Formato grande
     texto += "POLLOS EL SABROSITO\n";
-    texto += "\x1B\x45\x00"; // Negrilla OFF
+    texto += "\x1B\x46\x00"; // Negrilla OFF
     texto += "\x1B\x21\x00"; //vuelve al texto normal
     texto += "\x1B\x61\x00"; //aliniamiento a la izquierda
     texto += "----------------------\n";
